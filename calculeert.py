9fcf8dc26792361119a160810d6ea404aabdb9bc
amlan0013 <banerjeeriju653@gmail.com>
2021-12-05 18:01:19 -1000

    Create calculeert.py

---
+++ calculeert.py
@@ -0,0 +1,16 @@
+print("this is my first cretion")
+a=input("input th e1st number")
+a=int(a)
+b=input("input th second number")
+b=int(b)
+c=input("input your sign")
+if c=="+":
+    print("your sum is:",a+b)
+elif c=="-":
+    print("your sum is:",a-b)    
+elif c=="/":
+    print("your sum is:",a-b)        
+elif c=="*":
+    print("your sum is:",a-b) 
+else:
+    print("you input wrong sign")          
